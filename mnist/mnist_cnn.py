f8126d0406f37b1e0a76c32531f461a21f31dcbd
Wang Taiqing <taiqingwang@creditease.cn>
2016-03-10 15:19:17 +0800

    mnist_cnn: report validation accuracy during training

--- mnist/mnist_cnn.py
+++ mnist/mnist_cnn.py
@@ -57,9 +57,9 @@ if __name__ == '__main__':
         tf.initialize_all_variables().run()
         for i in range(10000):
             batch = mnist.train.next_batch(50)
-            if i%100 == 0:
-                train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
-                print("step %d, training accuracy %g"%(i, train_accuracy))
+            if i%500 == 0:
+                valid_accuracy = accuracy.eval(feed_dict={x: mnist.validation.images, y_: mnist.validation.labels, keep_prob: 1.0})
+                print("step %d, validation accuracy %g"%(i, valid_accuracy))
             train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
     
         test_accuracy = accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})
