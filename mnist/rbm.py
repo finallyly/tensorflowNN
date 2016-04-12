03fd6ffbb626d21fdd1553cc10300631d52bf5da
Wang Taiqing <taiqingwang@creditease.cn>
2016-04-12 15:42:15 +0800

    rbm.py: move model training configuration into __init__; iterate_dataset: randomly permutate samples in the first place

--- mnist/rbm.py
+++ mnist/rbm.py
@@ -12,58 +12,19 @@ from tf_util import Struct, GraphWrapper
 
 
 class RBM(object):
-    def __init__(self):
-        # 模型的参数,对应于Graph中的trainable variables
+    def __init__(self, n_hidden, gibbs_steps=1, batch_size=50, num_epochs=10000, learning_rate=1e-3, probe_epochs=50):
         self.params = {}
-        # Graph and session for online running
-        self.updated = False
-        self.G_run = None
-        self.sess_run = None
+        self.n_hidden = n_hidden
+        self.gibbs_steps = gibbs_steps
+        self.batch_size = batch_size
+        self.num_epochs = num_epochs
+        self.learning_rate = learning_rate
+        self.probe_epochs = probe_epochs
 
-    def __del__(self):
-        if self.sess_run is not None:
-            self.sess_run.close()
-
-    def __build_graph__(self, n_visible=None, n_hidden=None, var_val=None):
-        graph = tf.Graph()
-        with graph.as_default():
-            if var_val is not None:
-                n_visible = var_val['W'].shape[0]
-                n_hidden = var_val['W'].shape[1]
-                W = tf.Variable(var_val['W'])
-                b = tf.Variable(var_val['b'])
-                c = tf.Variable(var_val['c'])
-            else:
-                initial_W = np.float32(np.random.uniform(
-                    low=-4 * np.sqrt(6.0 / (n_hidden + n_visible)),
-                    high=4 * np.sqrt(6.0 / (n_hidden + n_visible)),
-                    size=(n_visible, n_hidden)
-                ))
-                W = tf.Variable(initial_value=initial_W, trainable=True)
-                b = tf.Variable(np.zeros((n_visible,), np.float32), trainable=True)
-                c = tf.Variable(np.zeros((n_hidden,), np.float32), trainable=True)
-            v = tf.placeholder(tf.float32, [None, n_visible])
-            v_sampling = tf.placeholder(tf.float32, [None, n_visible])
-            learning_rate = tf.placeholder(tf.float32)
-            loss = tf.reduce_mean(self.__calc_free_energy__(v, W, b, c)) - tf.reduce_mean(self.__calc_free_energy__(v_sampling, W, b, c))
-            train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss)
-            init_vars = tf.initialize_all_variables()
-        phr = Struct(v=v, v_sampling=v_sampling, learning_rate=learning_rate)
-        var = Struct(W=W, b=b, c=c)
-        tsr = Struct(loss=loss)
-        ops = Struct(train_step=train_step, init_vars=init_vars)
-        return GraphWrapper(graph, phr, var, tsr, ops)
-
-    def fit(self, v, n_hidden, gibbs_steps=1, batch_size=50, num_epochs=10000, learning_rate=1e-3, validation_v=None, probe_epochs=50):
+    def fit(self, v, validation_v=None):
         """
         :param v: 2d np.ndarray, each row stores a sample
-        :param n_hidden:
-        :param gibbs_steps:
-        :param batch_size:
-        :param num_epochs:
-        :param learning_rate:
         :param validation_v:
-        :param probe_epochs:
         :return:
         """
         if ((v == 0.0) | (v == 1.0)).sum() != v.shape[0] * v.shape[1]:
@@ -81,8 +42,8 @@ class RBM(object):
             dataset = self.iterate_dataset(v, batch_size)
             for i in range(num_epochs):
                 batch_v = dataset.next()
-                batch_v_sampling = self.gibbs_v(v0=batch_v, W=G.var.W.eval(), b=G.var.b.eval(), c=G.var.c.eval(), k=gibbs_steps)
-                if i % probe_epochs == 0:
+                batch_v_sampling = self.gibbs_v(v0=batch_v, W=G.var.W.eval(), b=G.var.b.eval(), c=G.var.c.eval(), k=self.gibbs_steps)
+                if i % self.probe_epochs == 0:
                     loss = G.tsr.loss.eval(feed_dict={G.phr.v: batch_v, G.phr.v_sampling: batch_v_sampling})
                     msg = 'step {i}, loss {l:.4f}'.format(i=i, l=loss)
                     if validation_v is not None:
@@ -91,26 +52,33 @@ class RBM(object):
                         mae = 1.0 * np.abs(reconstruct_v - validation_v).sum() / validation_v.shape[0]
                         msg += ", validation reconstruct MAE {e:.4f}".format(e=mae)
                     print msg
-                G.ops.train_step.run(feed_dict={G.phr.v: batch_v, G.phr.v_sampling: batch_v_sampling, G.phr.learning_rate: learning_rate})
+                G.ops.train_step.run(feed_dict={G.phr.v: batch_v, G.phr.v_sampling: batch_v_sampling, G.phr.learning_rate: self.learning_rate})
             for k, v in G.var.iteritems():
                 self.params[k] = G.var[k].eval()
         self.updated = True
 
-    def iterate_dataset(self, v, batch_size):
-        """
-        :param v: 2d np.array, each row stores an instance
-        :return:
-        """
-        i = 0
-        n_samples = v.shape[0]
-        indices = range(n_samples)
-        while True:
-            if i + batch_size > n_samples:
-                indices = np.random.permutation(n_samples)
-                i = 0
-            selected = indices[i : i + batch_size]
-            i += batch_size
-            yield v[selected]
+    def __build_graph__(self, n_visible, n_hidden):
+        graph = tf.Graph()
+        with graph.as_default():
+            initial_W = np.float32(np.random.uniform(
+                low=-4 * np.sqrt(6.0 / (n_hidden + n_visible)),
+                high=4 * np.sqrt(6.0 / (n_hidden + n_visible)),
+                size=(n_visible, n_hidden)
+            ))
+            W = tf.Variable(initial_value=initial_W, trainable=True)
+            b = tf.Variable(np.zeros((n_visible,), np.float32), trainable=True)
+            c = tf.Variable(np.zeros((n_hidden,), np.float32), trainable=True)
+            v = tf.placeholder(tf.float32, [None, n_visible])
+            v_sampling = tf.placeholder(tf.float32, [None, n_visible])
+            learning_rate = tf.placeholder(tf.float32)
+            loss = tf.reduce_mean(self.__calc_free_energy__(v, W, b, c)) - tf.reduce_mean(self.__calc_free_energy__(v_sampling, W, b, c))
+            train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+            init_vars = tf.initialize_all_variables()
+        phr = Struct(v=v, v_sampling=v_sampling, learning_rate=learning_rate)
+        var = Struct(W=W, b=b, c=c)
+        tsr = Struct(loss=loss)
+        ops = Struct(train_step=train_step, init_vars=init_vars)
+        return GraphWrapper(graph, phr, var, tsr, ops)
 
     def __calc_free_energy__(self, V, W, b, c):
         """
@@ -121,6 +89,22 @@ class RBM(object):
         :return: the free energy of each sample, 1d tensor, (N,)
         """
         return -tf.reshape(tf.matmul(V, tf.reshape(b, [-1, 1])), [-1]) - tf.reduce_sum(tf.log(1 + tf.exp(c + tf.matmul(V, W))), reduction_indices=1)
+
+    def iterate_dataset(self, v, batch_size):
+        """
+        :param v: 2d np.array, each row stores an instance
+        :return:
+        """
+        i = 0
+        n_samples = v.shape[0]
+        indices = np.random.permutation(n_samples)
+        while True:
+            if i + batch_size > n_samples:
+                indices = np.random.permutation(n_samples)
+                i = 0
+            selected = indices[i : i + batch_size]
+            i += batch_size
+            yield v[selected]
 
     def gibbs_v(self, v0, W, b, c, k=1):
         """
@@ -174,15 +158,15 @@ if __name__ == "__main__":
     mnist = input_data.read_data_sets("MNIST_data/", one_hot=False)
 
     n_hidden = 500
-    learning_rate = 1e-3
+    learning_rate = 1e-2
     gibbs_steps = 10
     batch_size = 100
-    num_epochs = 2000
+    num_epochs = 500
     probe_epochs = 50
-    rbm = RBM()
+    rbm = RBM(n_hidden=n_hidden, gibbs_steps=gibbs_steps, batch_size=batch_size, num_epochs=num_epochs, learning_rate=learning_rate, probe_epochs=probe_epochs)
     train_v = np.float32(mnist.train.images > 0)
     validation_v = np.float32(mnist.validation.images[np.random.permutation(mnist.validation.images.shape[0])][0:1000] > 0)
-    rbm.fit(train_v, validation_v=validation_v, n_hidden=n_hidden, gibbs_steps=gibbs_steps, batch_size=batch_size, num_epochs=num_epochs, learning_rate=learning_rate, probe_epochs=probe_epochs)
+    rbm.fit(train_v, validation_v)
     
     # sampling from the learnt distribution, starting from real samples
     gibbs_steps = 1
