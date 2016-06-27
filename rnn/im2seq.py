62e46b6eb36e7912fc638186ffae827a22cf2536
Wang Taiqing <taiqingwang@creditease.cn>
2016-06-27 10:36:35 +0800

    rnn: im2seq.py

---
+++ rnn/im2seq.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+import tensorflow as tf
+import numpy as np
+import cPickle
+import copy
+from PIL import Image
+from tensorflow.examples.tutorials.mnist import input_data
+
+if __name__ == '__main__':
+    mnist = input_data.read_data_sets("MNIST_data/", one_hot=False)
+    images = mnist.train.images
+    labels = mnist.train.labels
+    dataset = []
+    for i in range(0, images.shape[0]):
+        im = images[i, :].reshape((28, 28))
+        im = (im[0::2, 0::2] > 0.1).astype(int)
+        nonzeros = im.reshape((-1)).nonzero()[0]
+        dataset.append((nonzeros, labels[i]))
+        if i % 1000 == 0:
+            print '{i} samples processed'.format(i=i)
+    cPickle.dump(dataset, open("MNIST_data/mnist_seq.pkl", "wb"))
--- rnn/test.py
+++ rnn/test.py
@@ -11,13 +11,19 @@ if __name__ == '__main__':
     plt.close('all')
 
     mnist = input_data.read_data_sets("MNIST_data/", one_hot=False)
-    x = mnist.train.images
-    y = mnist.train.labels
-    im = x[0, :].reshape((28, 28))
-    im = 255 * (im > 0).astype(np.uint8)
-    im = np.stack((im, im, im), axis=2)
-    fig = plt.figure(0)
+    images = mnist.train.images
+    labels = mnist.train.labels
+    idx = 4
+    im = images[idx, :].reshape((28, 28))
+    im = (im[0::2, 0::2] > 0.1).astype(int)
+    x = im.reshape((-1))
+    non_zeros = x.nonzero()[0]
+    
+    im_vis = 255 * im.astype(np.uint8)
+    im_vis = np.stack((im_vis, im_vis, im_vis), axis=2)
+    fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.imshow(im)
+    ax.imshow(im_vis)
+    ax.set_title(str(labels[idx]))
     ax.axis('off')
     fig.show()
